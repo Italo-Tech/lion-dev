9d5f6d03569bf1ff820b21fdbd6fdf831e79ff81
Italo <italosantosdick@hotmail.com>
2025-07-08 16:46:55 -0300

    :sparkles: feat: Analytics adicionado.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next'
+import { Analytics } from '@vercel/analytics/react'
 import './globals.css'
 
 export const metadata: Metadata = {
@@ -31,7 +32,10 @@ export default function RootLayout({
         <link rel="icon" type="logo/png" href="/icon.png" />
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
       </head>
-      <body>{children}</body>
+      <body>
+        {children}
+        <Analytics />
+      </body>
     </html>
   )
 }
